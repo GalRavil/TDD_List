829762b2638ec09ef5166eaa539ee647199d0553
GalRavil <gal.ravil93@gmail.com>
2018-11-27 13:58:59 +0300

    + functional test for multiple lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,7 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_list_and_retrieve_it_later(self):
+    def test_can_start_list_for_one_user(self):
         # User1 checks home page
         self.browser.get(self.live_server_url)
 
@@ -64,8 +64,43 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('2: Buy item2')
 
 
-        # Check the unique URL
-        self.fail('Finish test after you sleep!')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # User1 starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy another stuff')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy another stuff')
 
-        # User1 visits that URL to make sure the to-do list is still there
-    
+        # Check the unique URL
+        user1_list_url = self.browser.current_url
+        self.assertRegex(user1_list_url, '/lists/.+')
+
+
+        # Now a new user, User2, comes along to the site
+        ## We use a new browser session to make sure no information
+        ## of User1 is coming thought from cookies and etc
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # User2 visits the home page. There is no sign of User1's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy another stuff', page_text)
+        self.assertNotIn('Buy item1', page_text)
+
+        # User2 starts a new list by entering a new item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # User2 gets his own unique URL
+        user2_list_url = self.browser.current_url
+        self.assertRegex(user2_list_url, '/lists/.+')
+        self.assertNotEqual(user2_list_url, user1_list_url)
+
+        # Again, there is no trace of User1's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy another stuff')
+        self.assertIn('Buy milk', page_text)
