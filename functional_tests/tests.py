1896d19f182d2397b6d5d50ba5e22382c2c68619
GalRavil <gal.ravil93@gmail.com>
2018-12-02 23:09:36 +0300

    + tests for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -104,3 +104,26 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy another stuff', page_text)
         self.assertIn('Buy milk', page_text)
+
+
+    def test_layout_and_style(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
+
+        # Check the input is centered
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
